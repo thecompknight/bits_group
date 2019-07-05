f7078a5703f72da7adae7374e1495724d9a149ff
Abhishek Kalai Raghavendra <abraghav@microsoft.com>
2019-07-05 13:39:50 +0530

    Added the skeleton of BSGraph

---
+++ ds_assignment_1/BSGraph.py
@@ -0,0 +1,23 @@
+class BSGraph:
+    ActMov=[]
+    edges=[[],[]]
+    
+    def readActMovfile(self, inputfile):
+        return 
+    
+    def displayActMov(self): 
+        return 
+    
+    def displayMoviesOfActor(self, actor):
+        return 
+    
+    def displayActorsOfMovie(self,movie):
+        return 
+    
+    def findMovieRelation(self, movA,movB): 
+        return 
+    
+    def findMovieTransRelation(self, movA, movB):
+        return
+    
+    
