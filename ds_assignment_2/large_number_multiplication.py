9c5704a674640e498f643e8a203f27090346cd6a
Abhishek KR <thecompknight@gmail.com>
2019-09-05 11:41:32 +0530

    fixed the bug and made the code generic to work for 2 unequal length operands

--- ds_assignment_2/large_number_multiplication.py
+++ ds_assignment_2/large_number_multiplication.py
@@ -11,6 +11,14 @@ def large_multiply(str_x,str_y, f):
         n = max(len(str_x),len(str_y))
         nby2 = floor(n / 2)
         
+        #use multiplication by 10 to bring numbers to same length if they are not already
+        x_padding = n-len(str_x)
+        y_padding = n-len(str_y)
+        
+        str_x = pad_with_zeros_to_right(str_x, n)
+        str_y = pad_with_zeros_to_right(str_y, n)
+        
+        
         #divide the numbers into 4 parts, 2 parts from each
         a = divide_by_10(str_x,nby2)
         b = mod_by_10(str_x,nby2)
@@ -30,7 +38,10 @@ def large_multiply(str_x,str_y, f):
          
         # writing n as 2*nby2 takes care of both even and odd n
         prod = add(add(multiply_by_10(ac, 2*nby2) , multiply_by_10(ad_plus_bc, nby2)), bd)
-
+        
+        #remove the extra padding added by 10 multiplication
+        prod=prod[:len(prod)-x_padding-y_padding]
+        
         return prod
 
 def divide_by_10(x,num_of_zeros):
@@ -60,12 +71,21 @@ def add(x,y):
         ans = str(sum%10) + ans
         carry = int(sum/10) 
         pos=pos-1
+    if carry>0:
+        ans=str(carry)+ans
     return ans
 
 def pad_with_zeros(x, n):
     if n > len(x):
         num_of_zeros = n-len(x)
         return "0"*num_of_zeros+x
+    else:
+        return x
+
+def pad_with_zeros_to_right(x, n):
+    if n > len(x):
+        num_of_zeros = n-len(x)
+        return x+"0"*num_of_zeros
     else:
         return x
 
