ede82c165d8a6bed342586c9ce7b4219b9b5047a
Abhishek KR <thecompknight@gmail.com>
2019-09-05 09:47:01 +0530

    code works purely based on string for any length from input

--- ds_assignment_2/large_number_multiplication.py
+++ ds_assignment_2/large_number_multiplication.py
@@ -1,36 +1,90 @@
 from math import floor
-def large_multiply(x,y):
-    str_x = str(x)
-    str_y = str(y)
+
+def large_multiply(str_x,str_y, f):
+    
+    #check for single digit
     if len(str_x)==1 or len(str_y)==1:
-        return x*y
+        return str(int(str_x)*int(str_y))
     else:
+        
+        #Identify the number of digits involved
         n = max(len(str_x),len(str_y))
         nby2 = floor(n / 2)
         
-        a = int(x / 10**(nby2))
-        b = x % 10**(nby2)
-        c = int(y / 10**(nby2))
-        d = y % 10**(nby2)
+        #divide the numbers into 4 parts, 2 parts from each
+        a = divide_by_10(str_x,nby2)
+        b = mod_by_10(str_x,nby2)
+        c = divide_by_10(str_y,nby2)
+        d = mod_by_10(str_y,nby2)
         
-        ac = large_multiply(a,c)
-        bd = large_multiply(b,d)
-        ad_plus_bc = large_multiply(a+b,c+d) - ac - bd
+        f.write("----------------------------\n")
+        f.write("Intermediate Values of A1, B1 after partition:\n")
+        f.write("----------------------------\n")
+        f.write("A:"+str_x+" A1: "+a+" A2: "+b+"\n")
+        f.write("B:"+str_y+" B1: "+c+" B2: "+d+"\n")
         
-            # this little trick, writing n as 2*nby2 takes care of both even and odd n
-        prod = ac * 10**(2*nby2) + (ad_plus_bc * 10**nby2) + bd
+        #multiply each parts seperately using recursive call
+        ac = large_multiply(a,c,f)
+        bd = large_multiply(b,d,f)
+        ad_plus_bc = add(str(large_multiply(a, d,f)),str(large_multiply(b, c,f)))
+         
+        # writing n as 2*nby2 takes care of both even and odd n
+        prod = add(add(multiply_by_10(ac, 2*nby2) , multiply_by_10(ad_plus_bc, nby2)), bd)
 
         return prod
 
+def divide_by_10(x,num_of_zeros):
+    return x[:len(x)-num_of_zeros]
+
+def mod_by_10(x,num_of_zeros):
+    return x[len(x)-num_of_zeros:]
+
+def multiply_by_10(x,num_of_zeros):
+    return x+"0"*num_of_zeros
+
+def add(x,y):
+    n = max(len(x),len(y))
+    
+    x = pad_with_zeros(x, n)
+    y = pad_with_zeros(y, n)
+    
+    pos = n-1
+    carry=0
+    ans=""
+    
+    #loop through the digits to add them individually
+    while(pos>=0):
+        x_digit = int(x[pos])
+        y_digit = int(y[pos])
+        sum = x_digit + y_digit + carry
+        ans = str(sum%10) + ans
+        carry = int(sum/10) 
+        pos=pos-1
+    return ans
+
+def pad_with_zeros(x, n):
+    if n > len(x):
+        num_of_zeros = n-len(x)
+        return "0"*num_of_zeros+x
+    else:
+        return x
+
 def main():
     lines = ""
+    f= open("outputPS2.txt","w")
+
     with open("inputPS2.txt", encoding='utf8') as fin:
         lines = fin.read().strip()
     splitLines = lines.split("\n")
     num1 = splitLines[0]
     num2 = splitLines[1]
-    ans=large_multiply(int(num1), int(num2))
-    print(ans)
+    f.write("1st Number, A:"+num1+"\n")
+    f.write("2nd Number, B:"+num2+"\n")
+    
+    ans=large_multiply(num1, num2, f)
+    f.write("----------------------------\n")
+    f.write("Result:> "+num1+" x "+num2+" = "+ans+"\n")
+    f.write("----------------------------\n")
     
 if __name__== "__main__":
     main()
